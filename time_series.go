3bf05e5cce359a01b2d344117393278158872b2c
Simon Klinkert <simon.klinkert@gmail.com>
2020-02-11 22:07:40 +0100

    Add time series daily adjusted

--- time_series.go
+++ time_series.go
@@ -31,11 +31,53 @@ type TimeSeriesData struct {
 	Volume uint64  `json:"5. volume,string"`
 }
 
+// TimeSeriesAdjusted - like TimeSeries, but inclused dividends and adjusted close
+type TimeSeriesAdjusted struct {
+	Metadata         TimeSeriesMetadata        `json:"Meta Data"`
+	TimeSeriesAdjusted  map[string]TimeSeriesAdjustedData `json:"Time Series (Daily)"`
+}
+
+// TimeSeriesAdjustedData - like TimeSeries, but inclused dividends and adjusted close
+type TimeSeriesAdjustedData struct {
+	Open   float64 `json:"1. open,string"`
+	High   float64 `json:"2. high,string"`
+	Low    float64 `json:"3. low,string"`
+	Close  float64 `json:"4. close,string"`
+	Volume uint64  `json:"6. volume,string"`
+	DividendAmount float64  `json:"7. dividend amount,string"`
+	SplitCoefficient float64  `json:"8. split coefficient,string"`
+}
+
 func toTimeSeries(buf []byte) (*TimeSeries, error) {
 	timeSeries := &TimeSeries{}
 	if err := json.Unmarshal(buf, timeSeries); err != nil {
 		return nil, err
 	}
+	return timeSeries, nil
+}
+
+func toTimeSeriesAdjusted(buf []byte) (*TimeSeriesAdjusted, error) {
+	timeSeries := &TimeSeriesAdjusted{}
+	if err := json.Unmarshal(buf, timeSeries); err != nil {
+		return nil, err
+	}
+	return timeSeries, nil
+}
+
+// TimeSeriesAdjusted fetches the time series for given symbol from API.
+// The order of dates in returned object is random because it's a map.
+func (c *Client) TimeSeriesAdjusted(symbol string, outputSize OutPutSize) (*TimeSeriesAdjusted, error) {
+	const interval = "TIME_SERIES_DAILY_ADJUSTED"
+	url := fmt.Sprintf("%s/query?function=%s&symbol=%s&apikey=%s&outputsize=%s", baseURL, interval, symbol, c.apiKey, outputSize)
+	body, err := c.makeHTTPRequest(url)
+	if err != nil {
+		return nil, err
+	}
+	timeSeries, err := toTimeSeriesAdjusted(body)
+	if err != nil {
+		return nil, fmt.Errorf("failed to parse response: %w", err)
+	}
+
 	return timeSeries, nil
 }
 
